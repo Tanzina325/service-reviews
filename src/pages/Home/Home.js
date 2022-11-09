c454841007c8f7807d90b4fbe304fa80393a1ccb
01792166684 <tanzinatania491991@gmail.com>
2022-11-09 18:41:56 +0600

    banner added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,19 @@ import React from 'react';
 const Home = () => {
     return (
         <div className='text-center'>
-         <button className="rounded-full px-12 text-amber-600 font-bold text-xl py-3 bg-slate-600">Provided service in All Metro cities</button>
+         <button className="rounded-full px-12 my-3 text-amber-600 font-bold text-xl py-3 bg-slate-600">Provided service in All Metro cities</button>
+         <div className='grid lg:grid-cols-2 gap-2 justify-evenly items-center'>
+            <div className='mx-auto'>
+                <h1 className=' font-extrabold text-2xl text-amber-600'>Authentic and Homemade  <br /> <span className='font-extrabold  text-red-600 text-2xl'>Food delivered to your Home</span></h1>
+                
+                
+                <p>Missing Home made food of your Authentic state food ? Your <br /> wait is over…! I bring Home made food at your door steps.</p>
+            </div>
+            <div className=''>
+            <img src="https://www.hmfoodwala.com/assets/images/home_banner.png" alt="" />
+            </div>
+            
+       </div>
         </div>
     );
 };
